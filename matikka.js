cc8424e7347eca7b9357bb7cf48feb284f7f7b49
Kasperi Vaarala <n1vaka00@students.oamk.fi>
2021-12-01 09:36:30 +0200

    lisää matikkaa

--- matikka.js
+++ matikka.js
@@ -12,11 +12,22 @@ document.getElementById('question1').innerHTML = nro1 + " " + "+" + " " + nro2;
 var answer1 = nro1 + nro2;
 // tehtävä 2
 document.getElementById('question2').innerHTML = nro3 + " " + "+" + " " + nro4;
-var answer1 = nro1 + nro2;
+var answer2 = nro3 + nro4;
 //tehtävä 3
 document.getElementById("question3").innerHTML = nro5 + " " + "-" + " " + nro6;
 var answer3 = nro5 - nro6;
 //tehtävä 4
 document.getElementById("question4").innerHTML = nro4 + " " + "*" + " " + nro1;
+var answer4 = nro4 * nro1;
 //tehtävä 5
 document.getElementById("question5").innerHTML = nro5 + " " + "*" + " " + nro6;
+var answer5 = nro5 * nro6;
+
+function calc(){
+    let rightAnswer1 = document.getElementById("answer1").value
+    if (rightAnswer1 == answer1) {
+         console.log ("Vähän oot hyvä")
+    } else {
+        console.log ("oot tyhmä")
+    }
+}
